3707383d50d5db8f357b63f8d97b737f4fc6628c
zxlmufc <zxlmufc@gmail.com>
2017-06-07 12:04:12 +0800

    pre-processing added

--- script/pre_processing_1.py
+++ script/pre_processing_1.py
@@ -1,14 +1,20 @@
 from script import utils
 import pandas as pd
+import os
+import re
 import joblib
 
-act_test_data = utils.extract_zip_file('/mnt/trident/xiaolan/python/kaggle/redhat/data/act_test.csv.zip')
-act_train_data = utils.extract_zip_file('/mnt/trident/xiaolan/python/kaggle/redhat/data/act_train.csv.zip')
-people_data = utils.extract_zip_file('/mnt/trident/xiaolan/python/kaggle/redhat/data/people.csv.zip')
+act_test_data = utils.extract_zip_file(os.path.join(utils.root_path, '/data/act_test.csv.zip'))
+act_train_data = utils.extract_zip_file(os.path.join(utils.root_path, '/data/act_train.csv.zip'))
+people_data = utils.extract_zip_file(os.path.join(utils.root_path, '/data/people.csv.zip'))
 act_train_data['tag'] = 'train'
 act_test_data['tag'] = 'test'
 act_test_data['outcome'] = -1
-
 act_all_data = pd.concat([act_train_data, act_test_data], axis=0)
 act_all_data['date'] = act_all_data['date'].astype('datetime64')
+
+act_all_data = act_all_data.rename(columns=lambda x: re.sub('char', 'act_char', x))
+people_data = people_data.rename(columns=lambda x: re.sub('char', 'usr_char', x))
+
+
 
--- script/utils.py
+++ script/utils.py
@@ -3,6 +3,8 @@ import pandas as pd
 import os
 
 
+root_path = "/mnt/trident/xiaolan/python/kaggle/redhat"
+
 def extract_zip_file(zip_path):
     zf = zipfile.ZipFile(zip_path)
     fp = zf.extract(zf.filelist[0].filename)
