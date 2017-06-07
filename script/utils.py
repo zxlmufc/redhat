93d3e2777e1979bf4aa44ae889979be795152507
zxlmufc <zxlmufc@gmail.com>
2017-06-07 21:56:36 +0800

    pre-processing added

--- script/utils.py
+++ script/utils.py
@@ -1,13 +1,20 @@
 import zipfile
 import pandas as pd
+import sys
 import os
-
 
 root_path = "/mnt/trident/xiaolan/python/kaggle/redhat"
 
+
 def extract_zip_file(zip_path):
     zf = zipfile.ZipFile(zip_path)
-    fp = zf.extract(zf.filelist[0].filename)
-    df = pd.read_csv(fp)
+    df = pd.read_csv(zf.open(zf.filelist[0].filename))
     return df
 
+
+def main():
+    print('Hello there', sys.argv[1])
+
+
+if __name__ == '__main__':
+    main()
